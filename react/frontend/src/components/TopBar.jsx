bae80a8ce68e00d066fef20c56a3e8ce213288ce
gorselguler <gorsel.guler@gmail.com>
2026-05-07 19:03:05 +0200

    Update TopBar.jsx

---
+++ react/frontend/src/components/TopBar.jsx
@@ -0,0 +1,8 @@
+function TopBar() {
+    return (
+        <div className="h-16 bg-indigo-700 text-white flex items-center px-6">
+            <h1 className="text-xl font-bold tracking-wide">Task Manager</h1>
+            </div>
+         );
+}
+export default TopBar;
