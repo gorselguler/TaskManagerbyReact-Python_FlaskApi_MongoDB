44f1ff031176adc5264ce8b98f8c34b6180034c2
gorselguler <gorsel.guler@gmail.com>
2026-05-07 19:12:09 +0200

    Update LeftSideBar.jsx

---
+++ react/frontend/src/components/LeftSideBar.jsx
@@ -0,0 +1,13 @@
+function LeftSidebar( ) {
+    return ( 
+        <div className="w-46 min-h-screen bg-indigo-900 text-indigo-200 flex-col p-4">
+          <p className="text-xs uppercase tracking-widest mb-4 text-indigo-400">Menu</p>
+          <a href="#" className="py-2 px-3 rounded-lg hover:bg-indigo-700 hover:text-white transition">
+            All Task 
+            </a>
+            </div>
+
+    );
+}
+
+export default LeftSidebar;
